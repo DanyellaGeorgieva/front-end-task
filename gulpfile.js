ff2229e49d88acd4089b9d0f812adeb832c9e7eb
Daniela Georgieva <danyellageorgieva@gmail.com>
2021-06-20 18:45:46 +0300

    init gulp boilerplate

---
+++ gulpfile.js
@@ -0,0 +1,50 @@
+const { src, dest, watch, series } = require('gulp');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
+
+const files = {
+	scssPath: 'src/scss/styles.scss',
+	jsPath: 'src/js/',
+};
+
+// Sass Task
+function scssTask(){
+  return src(files.scssPath, { sourcemaps: true })
+    .pipe(sass().on('error', sass.logError))
+    .pipe(dest('dist/css', { sourcemaps: '.' }));
+}
+
+// JavaScript Task
+function jsTask(){
+  return src(files.jsPath, { sourcemaps: true })
+    .pipe(dest('dist/js', { sourcemaps: '.' }));
+}
+
+// BrowserSync Tasks
+function browserSyncServe(cb){
+  browserSync.init({
+    server: {
+      baseDir: '.'
+    }
+  });
+  cb();
+}
+
+function browserSyncReload(cb){
+  browserSync.reload();
+  cb();
+}
+
+// Watch Task
+function watchTask(){
+  watch('*.html', browserSyncReload);
+  watch(['src/scss/styles.scss', 'src/js/main.js'], series(scssTask, jsTask, browserSyncReload));
+}
+
+// Default Gulp task
+exports.default = series(
+  scssTask,
+  jsTask,
+  browserSyncServe,
+  watchTask
+);
