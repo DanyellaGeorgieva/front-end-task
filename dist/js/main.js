46329fd3a33e6641f674eac78f25b7614490664e
Daniela Georgieva <danyellageorgieva@gmail.com>
2021-06-21 13:49:35 +0300

    adjusted responsive

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,13 +1,2 @@
-// import Swiper from 'swiper';
-// // import Swiper styles
-// // import 'swiper/swiper-bundle.css';
 
-// // const swiper = new Swiper();
-// // console.log(swiper);
-
-
-// import Swiper from 'swiper/bundle';
-
-// // init Swiper:
-// const swiper = new Swiper();
 //# sourceMappingURL=main.js.map
